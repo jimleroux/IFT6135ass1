7898f97e5db1c873fdc45020f975ec05fec78072
jimleroux <jim.leroux1@gmail.com>
2019-01-21 12:49:21 -0500

    Update assignment1.py

--- python/assignment1.py
+++ python/assignment1.py
@@ -30,25 +30,34 @@ class ConvNet(nn.Module):
 		self.convlayers = nn.Sequential(
 			nn.Conv2d(3, 64, 5, padding=0),
 			nn.ReLU(),
+			nn.BatchNorm2d(64),
 			nn.Conv2d(64, 64, 5, padding=0),
 			nn.ReLU(),
+			nn.BatchNorm2d(64),
 			nn.MaxPool2d(2, 2),
 			nn.Conv2d(64, 128, 5, padding=0),
 			nn.ReLU(),
+			nn.BatchNorm2d(128),
 			nn.Conv2d(128, 128, 5, padding=0),
 			nn.ReLU(),
+			nn.BatchNorm2d(128),
 			nn.MaxPool2d(2, 2),
 			nn.Conv2d(128, 256, 3, padding=0),
 			nn.ReLU(),
+			nn.BatchNorm2d(256),
 			nn.Conv2d(256, 256, 3, padding=1),
 			nn.ReLU(),
+			nn.BatchNorm2d(256),
 			nn.MaxPool2d(2, 2),
 			nn.Conv2d(256, 512, 3, padding=1),
-			nn.ReLU())
+			nn.ReLU(),
+			nn.BatchNorm2d(512),)
 		# 60 56 28 24 20 10
 		self.denses = nn.Sequential(
 			nn.Linear(512 * 4 * 4, 1024),
 			nn.ReLU(),
+			nn.BatchNorm1d(1024),
+			nn.Dropout2d(p=0.20)
 			nn.Linear(1024, 2))
 			
 	def forward(self, x):
