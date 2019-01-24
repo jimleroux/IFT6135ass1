f96e3cab008ce63fd59311686074ac3e6c14a925
jimleroux <jim.leroux1@gmail.com>
2019-01-24 13:43:39 -0500

    split functions

--- python/main.py
+++ python/main.py
@@ -41,93 +41,6 @@ class dataset(torch.utils.data.dataset.Dataset):
 	def __len__(self):
 		return len(self.data)
 
-def train(model, full_data, num_epoch=20, lr=0.1):
-		"""
-		Train function for the network.
-
-		Inputs:
-		-------
-		cnn: conv_net instance we want to train.
-		trainloader: pytorch Dataloader instance containing the training data.
-		testloader: pytorch Dataloader instance containing the test data.
-		num_epoch: number of training epoch.
-		lr: Learning rate for the SGD.
-
-		Returns:
-		-------
-		loss_train: normalized loss on the training data at after each epoch.
-		loss_valid: normalized loss on the test data at after each epoch.
-		err_train: total error on the training set after each epoch.
-		err_valid: total error on the test set after each epoch.
-		"""
-		split = [
-			int(0.8*len(full_data)),
-			len(full_data)-int(0.8*len(full_data))
-			]
-		train, valid = torch.utils.data.dataset.random_split(full_data, split)
-		trainloader = torch.utils.data.DataLoader(
-			train, batch_size=64, shuffle=True)
-		validloader = torch.utils.data.DataLoader(
-			valid, batch_size=64, shuffle=True)
-
-		criterion = nn.CrossEntropyLoss()
-		
-		loss_train = []
-		loss_valid = []
-		err_train = []
-		err_valid = []
-		for epoch in range(num_epoch):
-			model.train()
-			lrd = lr / (1 + 9*epoch/num_epoch)
-			optimizer = optim.SGD(model.parameters(), lr=lr)		
-			for datas in trainloader:
-				inputs, labels = datas
-				optimizer.zero_grad()
-				outputs = model(inputs.to(device))
-				loss = criterion(outputs, labels.to(device))
-				loss.backward()
-				optimizer.step()
-
-			running_loss_train = 0.0
-			running_loss_valid = 0.0
-			correct = 0.
-			total = 0.
-			model.eval()
-			with torch.no_grad():
-				for datas in trainloader:
-					inputs, labels = datas
-					outputs = model(inputs.to(device))
-					_, predicted = torch.max(outputs.data, 1)
-					total += labels.size(0)
-					correct += (predicted == labels.to(device)).sum().item()
-					loss = criterion(outputs, labels.to(device))
-					running_loss_train += loss.item() / len(train)
-			err_train.append(1 - correct / total)
-			
-			correct = 0.
-			total = 0.
-			with torch.no_grad():
-				for datas in validloader:
-					inputs, labels = datas
-					outputs = model(inputs.to(device))
-					_, predicted = torch.max(outputs.data, 1)
-					total += labels.size(0)
-					correct += (predicted == labels.to(device)).sum().item()
-					loss = criterion(outputs, labels.to(device))
-					running_loss_valid += loss.item() / len(valid)
-			err_valid.append(1 - correct / total)
-			
-			loss_train.append(running_loss_train)
-			loss_valid.append(running_loss_valid)
-			print('Epoch: {}'.format(epoch))
-			print('Train loss: {0:.4f} Train error: {1:.2f}'.format(
-				loss_train[epoch], err_train[epoch]))
-			print('Test loss: {0:.4f} Test error: {1:.2f}'.format(
-			   loss_valid[epoch], err_valid[epoch]))
-
-		print('Finished Training')
-		return loss_train, loss_valid, err_train, err_valid
-
 if __name__ == '__main__':
 	device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 	torch.cuda.manual_seed(10)
@@ -158,8 +71,8 @@ if __name__ == '__main__':
 	MLP = mlp.NN()
 	atrain, atest = MLP.train(trainloader,testloader)
 	n_epoch = 70
-	#loss_train, loss_valid, e_train, e_valid = train(
-	#	CNN, cat_dog_data, num_epoch=n_epoch)
+	loss_train, loss_valid, e_train, e_valid = CNN.train(
+		cat_dog_data, num_epoch=n_epoch)
 	
 
 	#plt.figure()
