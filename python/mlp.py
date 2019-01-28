2d64751a975dbb590c6971d2a9863bfff5861803
jimleroux <jim.leroux1@gmail.com>
2019-01-28 15:33:06 -0500

    Update mlp.py

--- python/mlp.py
+++ python/mlp.py
@@ -140,7 +140,7 @@ class NN(object):
             self.parameters[par] -= lr * grads["d"+par]
 
     def train(self, trainloader, testloader,
-              num_epoch=10, lr=0.1, lam=0.0001):
+              num_epoch=10, lr=0.0001, lam=0.0001):
         acc_train = []
         acc_test = []
         for epoch in range(num_epoch):
