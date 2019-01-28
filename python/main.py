97f705e9e9fc7024a85a5c3b5b4097d005a48e33
jimleroux <jim.leroux1@gmail.com>
2019-01-27 23:12:00 -0500

    pep8 modification

--- python/main.py
+++ python/main.py
@@ -14,81 +14,80 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-import time
-import csv
 import os
 import cnn
 import mlp
 if torch.cuda.is_available():
-	import cupy as np
+    import cupy as np
 else:
-	import numpy as np
+    import numpy as np
 
 CURRENT_DIR = os.getcwd()
 
+
 class dataset(torch.utils.data.dataset.Dataset):
-	def __init__(self, transforms=None):
-		os.chdir("../dataset/trainset/")
-		self.data = torchvision.datasets.ImageFolder(
-			os.getcwd(), transform=transforms)
-		os.chdir(CURRENT_DIR)
 
-	def __getitem__(self, idx):
-		return self.data[idx]
+    def __init__(self, transforms=None):
+        os.chdir("../dataset/trainset/")
+        self.data = torchvision.datasets.ImageFolder(
+            os.getcwd(), transform=transforms)
+        os.chdir(CURRENT_DIR)
 
-	def __len__(self):
-		return len(self.data)
+    def __getitem__(self, idx):
+        return self.data[idx]
+
+    def __len__(self):
+        return len(self.data)
+
 
 if __name__ == '__main__':
-	parser = argparse.ArgumentParser()
-	parser.add_argument(
-		"-m", "--model", help="Choose mlp or cnn, default is both", type=str)
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-m", "--model", help="Choose mlp or cnn, default is both", type=str)
+    args = parser.parse_args()
 
-	device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-	torch.cuda.manual_seed(10)
-	plt.style.use('ggplot')
-	plt.rc('xtick', labelsize=15)
-	plt.rc('ytick', labelsize=15)
-	plt.rc('axes', labelsize=15)
-	
-	# Define the transformations.
-	transform = transforms.Compose([
-		transforms.RandomHorizontalFlip(),
-		transforms.RandomVerticalFlip(),transforms.ToTensor(),
-		transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-		])
-	
-	# Create, load and split the datas.
-	cat_dog_data = dataset(transforms=transform)
-	mnist_train = torchvision.datasets.MNIST(
-		root='./data', train=True, download=True, transform=transform)
-	trainloader = torch.utils.data.DataLoader(
-		mnist_train,batch_size=256,shuffle=True)
-	mnist_test = torchvision.datasets.MNIST(
-		root='./data', train=False, download=True, transform=transform)	
-	testloader = torch.utils.data.DataLoader(
-		mnist_test,batch_size=256,shuffle=False)	
-	# Create the models
-	CNN = cnn.ConvNet().to(device)
-	MLP = mlp.NN()
-	
-	if args.model is None:
-		acc_train, acc_test = MLP.train(trainloader,testloader)
-		loss_train, loss_valid, e_train, e_valid = CNN.train_(
-			cat_dog_data, device)
-	elif args.model == "mlp":
-		acc_train, acc_test = MLP.train(trainloader,testloader)
-	elif args.model == "cnn":
-		loss_train, loss_valid, e_train, e_valid = CNN.train_(
-			cat_dog_data, device)
-	#plt.figure()
-	#plt.plot(range(1,n_epoch+1),e_train, 'sk-', label='Train')
-	#plt.plot(range(1, n_epoch+1),e_valid, 'sr-', label='Valid')
-	#plt.xlabel('Epoch')
-	#plt.ylabel('Error')
-	#plt.legend(fontsize=25)
-	#plt.show()
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    torch.cuda.manual_seed(10)
+    plt.style.use('ggplot')
+    plt.rc('xtick', labelsize=15)
+    plt.rc('ytick', labelsize=15)
+    plt.rc('axes', labelsize=15)
+
+    # Define the transformations.
+    transform = transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomVerticalFlip(), transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+
+    # Create, load and split the datas.
+    cat_dog_data = dataset(transforms=transform)
+    mnist_train = torchvision.datasets.MNIST(
+        root='./data', train=True, download=True, transform=transform)
+    trainloader = torch.utils.data.DataLoader(
+        mnist_train, batch_size=256, shuffle=True)
+    mnist_test = torchvision.datasets.MNIST(
+        root='./data', train=False, download=True, transform=transform)
+    testloader = torch.utils.data.DataLoader(
+        mnist_test, batch_size=256, shuffle=False)
+    # Create the models
+
+    CNN = cnn.ConvNet().to(device)
+    MLP = mlp.NN()
+
+    if args.model is None:
+        acc_train, acc_test = MLP.train(trainloader, testloader)
+        loss_train, loss_valid, e_train, e_valid = CNN.train_(
+            cat_dog_data, device)
+    elif args.model == "mlp":
+        acc_train, acc_test = MLP.train(trainloader, testloader)
+    elif args.model == "cnn":
+        loss_train, loss_valid, e_train, e_valid = CNN.train_(
+            cat_dog_data, device)
+    # plt.figure()
+    # plt.plot(range(1,n_epoch+1),e_train, 'sk-', label='Train')
+    # plt.plot(range(1, n_epoch+1),e_valid, 'sr-', label='Valid')
+    # plt.xlabel('Epoch')
+    # plt.ylabel('Error')
+    # plt.legend(fontsize=25)
+    # plt.show()
--- python/mlp.py
+++ python/mlp.py
@@ -9,182 +9,179 @@ __maintainer__ = "Jimmy Leroux"
 __studentid__ = "1024610"
 
 import torch
-import torchvision
-import torchvision.transforms as transforms
-import matplotlib.pyplot as plt
-import torch.nn as nn
-import torch.nn.functional as F
-import torch.optim as optim
-import time
-import csv
-import os
 if torch.cuda.is_available():
-	import cupy as np
+    import cupy as np
 else:
-	import numpy as np
+    import numpy as np
+
 
 class NN(object):
-	
-	def __init__(
-			self, hidden_dims=(1024,2048), n_hidden=2, mode='train',
-			datapath=None, model_path=None):
-		
-		self.n_hidden = n_hidden
-		self.hidden_dims = hidden_dims
-		self.parameters = {}
-		self.layers = [28*28, 512, 512, 10]
-		self.initialize_weights(n_hidden, 0)
 
-	def initialize_weights(self, n_hidden, dims):
-		num_layer = len(self.layers)
-		for i in range(1, num_layer):
-			n_c = 1. / np.sqrt(self.layers[i])
-			self.parameters["W"+str(i)] = np.ones(
-				(self.layers[i], self.layers[i-1])) * np.random.uniform(
-				-n_c, n_c, (self.layers[i], self.layers[i-1]))
-			self.parameters["b"+str(i)] = np.zeros((self.layers[i], 1))
+    def __init__(
+            self, hidden_dims=(1024, 2048), n_hidden=2, mode='train',
+            datapath=None, model_path=None):
 
-	def forward(self, X):
-		"""
-		Forward propagation method. It propagated X through the network.
+        self.n_hidden = n_hidden
+        self.hidden_dims = hidden_dims
+        self.parameters = {}
+        self.layers = [28*28, 512, 512, 10]
+        self.initialize_weights(n_hidden, 0)
 
-		Parameters:
-		-----------
-		X: Input matrix we wish to propagate. Shape: (dim, num_exemple)
+    def initialize_weights(self, n_hidden, dims):
+        num_layer = len(self.layers)
+        for i in range(1, num_layer):
+            n_c = 1. / np.sqrt(self.layers[i])
+            self.parameters["W"+str(i)] = np.ones(
+                (self.layers[i], self.layers[i-1])) * np.random.uniform(
+                -n_c, n_c, (self.layers[i], self.layers[i-1]))
+            self.parameters["b"+str(i)] = np.zeros((self.layers[i], 1))
 
-		Returns:
-		cache: Dictionary of the intermediate cache at each step of the propagation.
-		"""
+    def forward(self, X):
+        """
+        Forward propagation method. It propagated X through the network.
 
-		h1 = np.dot(self.parameters["W1"], X) + self.parameters["b1"]
-		a1 = self.activation(h1)
-		h2 = np.dot(self.parameters["W2"], a1) + self.parameters["b2"]
-		a2 = self.activation(h2)
-		h3 = np.dot(self.parameters["W3"], a2) + self.parameters["b3"]
-		a3 = self.softmax(h3)
-		cache = {"h1":h1, "a1":a1, "h2":h2, "a2":a2, "h3":h3, "a3":a3, "X":X}
-		return cache
+        Parameters:
+        -----------
+        X: Input matrix we wish to propagate. Shape: (dim, num_exemple)
 
-	def activation(self, X):
-		return np.maximum(0, X)
+        Returns:
+        cache: Dictionary of the intermediate cache at each step
+                of the propagation.
+        """
 
-	def loss(self, Y, cache, lam):
-		loss = np.sum(-np.log(cache["a3"])*Y)
-		loss += lam*np.sum(np.abs(self.parameters["W1"]))\
-				+ lam*np.sum(np.abs(self.parameters["W2"]))\
-				+ lam*np.sum(np.abs(self.parameters["W3"]))\
-				+ lam*np.sum(self.parameters["W1"]**2)\
-				+ lam*np.sum(self.parameters["W2"]**2)\
-				+ lam*np.sum(self.parameters["W3"]**2)
-		loss *= X.shape[1]
-		return loss
+        h1 = np.dot(self.parameters["W1"], X) + self.parameters["b1"]
+        a1 = self.activation(h1)
+        h2 = np.dot(self.parameters["W2"], a1) + self.parameters["b2"]
+        a2 = self.activation(h2)
+        h3 = np.dot(self.parameters["W3"], a2) + self.parameters["b3"]
+        a3 = self.softmax(h3)
+        cache = {
+                "h1": h1, "a1": a1, "h2": h2, "a2": a2,
+                "h3": h3, "a3": a3, "X": X
+                }
+        return cache
 
-	def softmax(self, X):
-		max_ = X.max(axis=0)
-		out = np.exp(X-max_) / np.sum(np.exp(X-max_), axis=0)
-		return out
+    def activation(self, X):
+        return np.maximum(0, X)
 
-	def backward(self, cache, Y, lam):
-		"""
-		Method performing the backpropagation for the model.
-		
-		Parameters:
-		-----------
-		cache: Stored intermediate cache of the forward propagation pass.
-		Y: Target cache (of the training set). Shape: (num_class, num_exemple)
+    def loss(self, Y, cache, lam):
+        loss = np.sum(-np.log(cache["a3"])*Y)
+        loss += lam*np.sum(np.abs(self.parameters["W1"]))\
+            + lam*np.sum(np.abs(self.parameters["W2"]))\
+            + lam*np.sum(np.abs(self.parameters["W3"]))\
+            + lam*np.sum(self.parameters["W1"]**2)\
+            + lam*np.sum(self.parameters["W2"]**2)\
+            + lam*np.sum(self.parameters["W3"]**2)
+        loss *= cache["X"].shape[1]
+        return loss
 
-		Returns:
-		--------
-		grads: Dictionary containing the gradients of the parameters.
+    def softmax(self, X):
+        max_ = X.max(axis=0)
+        out = np.exp(X-max_) / np.sum(np.exp(X-max_), axis=0)
+        return out
 
-		"""
+    def backward(self, cache, Y, lam):
+        """
+        Method performing the backpropagation for the model.
 
-		grads = {}
+        Parameters:
+        -----------
+        cache: Stored intermediate cache of the forward propagation pass.
+        Y: Target cache (of the training set). Shape: (num_class, num_exemple)
 
-		dh3 = cache["a3"] - Y
-		dW3 = np.mean(dh3[:,None,:]*cache["a2"][None,:,:], axis=2)\
-				+ 2*lam*self.parameters["W3"]\
-				+ lam*np.sign(self.parameters["W3"])
-		db3 = np.mean(dh3, axis=1, keepdims=True)
-		
-		da2 = np.sum(dh3[:,None,:]*self.parameters["W3"][:,:,None], axis=0)
-		dh2 = da2*1.*(cache["a2"]>0)
-		dW2 = np.mean(dh2[:,None,:]*cache["a1"][None,:,], axis=2)\
-				+ 2*lam*self.parameters["W2"]\
-				+ lam*np.sign(self.parameters["W2"])
-		db2 = np.mean(dh2, axis=1, keepdims=True)
+        Returns:
+        --------
+        grads: Dictionary containing the gradients of the parameters.
 
-		da1 = np.sum(dh2[:,None,:]*self.parameters["W2"][:,:,None], axis=0)
-		dh1 = da1*1.*(cache["a1"]>0)
-		dW1 = np.mean(dh1[:,None,:]*cache["X"][None,:,], axis=2)\
-				+ 2*lam*self.parameters["W1"]\
-				+ lam*np.sign(self.parameters["W1"])
-		db1 = np.mean(dh1, axis=1, keepdims=True)
-		
-		dx = np.sum(dh1[:,None,:]*self.parameters["W1"][:,:,None], axis=0)
-		grads = {
-			"dW3":dW3, "db3":db3, "dW2":dW2, "dW1":dW1,
-			"db2":db2, "db1":db1, "dx":dx
-			}
-		return grads
+        """
 
-	def update(self, grads, lr):
-		for par in self.parameters.keys():
-			self.parameters[par] -= lr * grads["d"+par]  
+        grads = {}
 
-	def train(self, trainloader, testloader, num_epoch=10, lr=0.01, lam=0.0001):
-		acc_train = []
-		acc_test = []
-		for epoch in range(num_epoch):
-			print("Epoch:{}".format(epoch))
-			for data in trainloader:
-				inputs, labels = data
-				inputs = self.transform_input(inputs)
-				labels = self.onehot(labels)
-				cache = self.forward(inputs)
-				grads = self.backward(cache, labels, lam)
-				self.update(grads, lr)
+        dh3 = cache["a3"] - Y
+        dW3 = np.mean(dh3[:, None, :]*cache["a2"][None, :, :], axis=2)\
+            + 2*lam*self.parameters["W3"]\
+            + lam*np.sign(self.parameters["W3"])
+        db3 = np.mean(dh3, axis=1, keepdims=True)
 
-			# Calculate the accuracy
-			correct = 0.
-			total = 0.
-			for data in trainloader:
-				inputs, labels = data
-				inputs = self.transform_input(inputs)				
-				labels = self.onehot(labels)
-				cache = self.forward(inputs)
-				preds = np.argmax(cache["a3"], axis=0)
-				correct += np.sum(preds==np.argmax(labels, axis=0))
-				total += labels.shape[1]
-				if total>5000:
-					break
-			acc_train.append(float(correct/total)*100)
+        da2 = np.sum(dh3[:, None, :]*self.parameters["W3"][:, :, None], axis=0)
+        dh2 = da2*1.*(cache["a2"] > 0)
+        dW2 = np.mean(dh2[:, None, :]*cache["a1"][None, :, :], axis=2)\
+            + 2*lam*self.parameters["W2"]\
+            + lam*np.sign(self.parameters["W2"])
+        db2 = np.mean(dh2, axis=1, keepdims=True)
 
-			correct = 0.
-			total = 0.
-			for data in testloader:
-				inputs, labels = data
-				inputs = self.transform_input(inputs)				
-				labels = self.onehot(labels)
-				cache = self.forward(inputs)
-				preds = np.argmax(cache["a3"], axis=0)
-				correct += np.sum(preds==np.argmax(labels, axis=0))
-				total += labels.shape[1]
-				if total>5000:
-					break
-			acc_test.append(float(correct/total)*100)
-			print("Accuracy train: {0:.2f}, Accuracy valid: {1:.2f}".format(
-				acc_train[epoch], acc_test[epoch]))
-		return acc_train, acc_test
+        da1 = np.sum(dh2[:, None, :]*self.parameters["W2"][:, :, None], axis=0)
+        dh1 = da1*1.*(cache["a1"] > 0)
+        dW1 = np.mean(dh1[:, None, :]*cache["X"][None, :, :], axis=2)\
+            + 2*lam*self.parameters["W1"]\
+            + lam*np.sign(self.parameters["W1"])
+        db1 = np.mean(dh1, axis=1, keepdims=True)
 
-	def test(self):
-		pass
+        dx = np.sum(dh1[:, None, :]*self.parameters["W1"][:, :, None], axis=0)
+        grads = {
+            "dW3": dW3, "db3": db3, "dW2": dW2, "dW1": dW1,
+            "db2": db2, "db1": db1, "dx": dx
+            }
+        return grads
 
-	def onehot(self, Y):
-		onehot = np.zeros((self.layers[-1],len(Y)))
-		for j in range(len(Y)):
-			onehot[int(Y[j]),j] = 1.
-		return onehot
+    def update(self, grads, lr):
+        for par in self.parameters.keys():
+            self.parameters[par] -= lr * grads["d"+par]
 
-	def transform_input(self, X):
-		return np.array(X.reshape((X.shape[0],self.layers[0])).numpy().T)
+    def train(self, trainloader, testloader,
+              num_epoch=10, lr=0.01, lam=0.0001):
+        acc_train = []
+        acc_test = []
+        for epoch in range(num_epoch):
+            print("Epoch:{}".format(epoch))
+            for data in trainloader:
+                inputs, labels = data
+                inputs = self.transform_input(inputs)
+                labels = self.onehot(labels)
+                cache = self.forward(inputs)
+                grads = self.backward(cache, labels, lam)
+                self.update(grads, lr)
+
+            # Calculate the accuracy
+            correct = 0.
+            total = 0.
+            for data in trainloader:
+                inputs, labels = data
+                inputs = self.transform_input(inputs)
+                labels = self.onehot(labels)
+                cache = self.forward(inputs)
+                preds = np.argmax(cache["a3"], axis=0)
+                correct += np.sum(preds == np.argmax(labels, axis=0))
+                total += labels.shape[1]
+                if total > 5000:
+                    break
+            acc_train.append(float(correct/total)*100)
+
+            correct = 0.
+            total = 0.
+            for data in testloader:
+                inputs, labels = data
+                inputs = self.transform_input(inputs)
+                labels = self.onehot(labels)
+                cache = self.forward(inputs)
+                preds = np.argmax(cache["a3"], axis=0)
+                correct += np.sum(preds == np.argmax(labels, axis=0))
+                total += labels.shape[1]
+                if total > 5000:
+                    break
+            acc_test.append(float(correct/total)*100)
+            print("Accuracy train: {0:.2f}, Accuracy valid: {1:.2f}".format(
+                acc_train[epoch], acc_test[epoch]))
+        return acc_train, acc_test
+
+    def test(self):
+        pass
+
+    def onehot(self, Y):
+        onehot = np.zeros((self.layers[-1], len(Y)))
+        for j in range(len(Y)):
+            onehot[int(Y[j]), j] = 1.
+        return onehot
+
+    def transform_input(self, X):
+        return np.array(X.reshape((X.shape[0], self.layers[0])).numpy().T)
