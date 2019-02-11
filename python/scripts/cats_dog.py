0220269308ddc8f87319957ca078e4ae9c110b83
jimleroux <jim.leroux1@gmail.com>
2019-02-11 15:34:05 -0500

    project modulation

---
+++ python/scripts/cats_dog.py
@@ -0,0 +1,46 @@
+import argparse
+import os
+import sys
+sys.path.insert(0, "../models")
+sys.path.insert(0, "../datasets")
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+from dataset import Dataset
+from dataset import data_split
+from cnn import ConvNet
+
+def main(args):
+    epoch = args.epoch
+    lr = args.lr
+    batch = args.batch
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    torch.cuda.manual_seed(10)
+
+    transform = transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomVerticalFlip(), transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+    
+    cat_dog_data = Dataset(transforms=transform)
+    train, valid = data_split(cat_dog_data)
+    cnn_cd = ConvNet("cat_and_dogs").to(device)
+    out = cnn_cd.train_(
+        train, valid, device, num_epoch=epoch, lr=lr, batchsize=batch)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--epoch", help="Choose the number of epoch",
+        default=70, type=int)
+    parser.add_argument(
+        "--lr", help="Choose the learning rate",
+        default=0.01, type=float)
+    parser.add_argument(
+        "--batch", help="Choose batchsize",
+        type=int)
+    args = parser.parse_args()
+    main(args)
---
+++ python/scripts/mlp_mnist.py
@@ -0,0 +1,46 @@
+import argparse
+import os
+import sys
+sys.path.insert(0, "../models")
+sys.path.insert(0, "../datasets")
+
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+from dataset import Dataset
+from dataset import data_split
+from mlp import NeuralNetwork
+from dataset import import_mnist
+import numpy as np
+
+def main(args):
+    epoch = args.epoch
+    lr = args.lr
+    batch = args.batch
+    np.random.seed(10)
+    
+    transform = transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomVerticalFlip(), transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ])
+
+    train, valid = import_mnist(transform)
+    mlp = NeuralNetwork()
+    out = mlp.train(
+        train, valid, num_epoch=epoch, lr=lr, batchsize=batch)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--epoch", help="Choose the number of epoch",
+        default=70, type=int)
+    parser.add_argument(
+        "--lr", help="Choose the learning rate",
+        default=0.01, type=float)
+    parser.add_argument(
+        "--batch", help="Choose batchsize",
+        default=256, type=int)
+    args = parser.parse_args()
+    main(args)
