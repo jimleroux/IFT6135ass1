90ef1be590acb0a5681d0c159d3ff167a8a7ec3c
jimleroux <jim.leroux1@gmail.com>
2019-02-14 15:28:53 -0500

    few modif

--- python/scripts/cats_dog.py
+++ python/scripts/cats_dog.py
@@ -7,7 +7,7 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 
-from datasets.dataset import Dataset, data_split
+from datasets.dataset import Dataset, data_split, TestDataset
 from models.cnn import ConvNet
 
 
@@ -25,10 +25,12 @@ def main(args):
         ])
     
     cat_dog_data = Dataset(transforms=transform)
+    submission = TestDataset()
     train, valid = data_split(cat_dog_data)
-    cnn_cd = ConvNet("cat_and_dogs").to(device)
-    out = cnn_cd.train_(
+    cnn = ConvNet("cat_and_dogs").to(device)
+    out = cnn.train_(
         train, valid, device, num_epoch=epoch, lr=lr, batchsize=batch)
+    cnn.prediction(submission, batchsize)
     return out
 
 if __name__ == "__main__":
